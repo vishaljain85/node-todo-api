4a86e241b1ed90e94d493e1b502336b26f41e77d
Vishal Jain <vjain@sportstg.com>
2018-09-29 23:16:24 +1000

    setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MOGODB_URI || 'mongodb://localhost:27017/TodoApp');
+//mongodb://admin:welcome1@ds051585.mlab.com:51585/nodejsapidb
 
 module.exports = {mongoose};
