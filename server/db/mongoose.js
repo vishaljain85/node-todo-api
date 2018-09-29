fc86f66870829b257ebfd8ee9ee97a52ac0027fc
Vishal Jain <vjain@sportstg.com>
2018-09-29 23:26:06 +1000

    corrected mongodb url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //mongoose.connect(process.env.MOGODB_URI || 'mongodb://localhost:27017/TodoApp');
-mongoose.connect('process.env.MOGODB_URI' || 'mongodb://admin:welcome1@ds051585.mlab.com:51585/nodejsapidb');
+mongoose.connect(process.env.MOGODB_URI || 'mongodb://admin:welcome1@ds051585.mlab.com:51585/nodejsapidb');
 //mongodb://admin:welcome1@ds051585.mlab.com:51585/nodejsapidb
 
 module.exports = {mongoose};
